791f7a264eec9686fa2ac39ff4b5c28ef32bc5ec
martinolanza03 <lanzamartino3@gmail.com>
2025-02-24 16:19:17 +0100

    fix: fix cost ticket

--- Js/app.js
+++ Js/app.js
@@ -1,19 +1,27 @@
 let km = prompt ('Quanti chilometri devi percorrere?');
 
 km = parseInt(km);
+console.log(km)
 
 let year = prompt ('Quanti anni hai?');
 
 year = parseInt(year);
+console.log(year)
 
 let costTicket;
+
+let discount;
 
 if (year >= 18 && year <= 64) {
     costTicket = km * 0.21;
 } else if (year <= 17) {
-    costTicket = ((km * 0.21) * 20) / 100;
+    costTicket = (km * 0.21);
+    discount = (costTicket * 20) /100;
+    costTicket = costTicket - discount;
 } else if (year >= 65) {
-    costTicket = ((km * 0.21) * 40) / 100;
+    costTicket = km * 0.21;
+    discount = (costTicket * 40) /100;
+    costTicket = costTicket - discount;
 }
 
 costTicket = costTicket.toFixed(2);
